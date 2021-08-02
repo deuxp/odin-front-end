f1d26a85297af8938d44bab19f448c2fd44ed45c
deuxp <deuxp@tutanota.com>
2021-08-02 16:52:09 -0400

    added

--- first-site/scripts/main.js
+++ first-site/scripts/main.js
@@ -1,2 +1,10 @@
 const myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
+
+// the function  is called query selector
+// which passed the parameter h1 to grab a reference to
+// the header and stored it in a variable called myHeading
+//similar to using a selector in CSS -> to do something
+// to an element you must select it first
+// then it set the value of the variables textproperty 
+// to hello world.
